402c162dc6b9cfc5b11f1ac6694e7a659085e8e0
Birtea Bogdan <bogdanbirtea122@gmail.com>
2020-07-13 01:43:10 +0300

    BB : goodbyePage.html updated, dar nu cum trebuie...

---
+++ src/main/java/com/example/Controller/DirectoryController.java
@@ -0,0 +1,28 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DirectoryController {
+
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public ModelAndView helloRequestMapping() {
+		ModelAndView mav = new ModelAndView();
+		System.out.println("Hello from helloRequestMapping");
+		mav.setViewName("helloPage.html");
+		return mav;
+	}
+	
+	
+	@RequestMapping(value="/goodbye", method=RequestMethod.GET)
+	public ModelAndView goodbyeRequestMapping() {
+		ModelAndView mav = new ModelAndView();
+		System.out.println("Good bye from goodbyeRequestMapping");
+		mav.setViewName("goodbyePage");
+		return mav;
+	}
+	
+}
---
+++ src/main/java/com/example/TelephoneDirectoryApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephoneDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephoneDirectoryApplication.class, args);
+		System.out.println("gata");
+	}
+
+}
